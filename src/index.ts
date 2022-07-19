4c01b9222f2a6a1ec84fdfbba18f5be6f825cf81
Deivid Gabriel <deividgabrielproc@gmail.com>
2022-07-19 19:53:11 -0300

    update: First commit

---
+++ src/index.ts
@@ -0,0 +1,31 @@
+import 'dotenv/config';
+import express from 'express';
+import path from 'path';
+import http from 'http';
+import { Server } from 'socket.io';
+
+const app = express();
+const server = http.createServer(app);
+const PORT = 3000;
+const PUBLIC_FOLDER = path.join(__dirname, '..', 'public/');
+const io = new Server(server);
+
+app.use(express.static(PUBLIC_FOLDER));
+
+app.get('/', (_req, res) => {
+  res.sendFile(path.join(PUBLIC_FOLDER, 'index.html'));
+});
+
+io.on('connection', (socket) => {
+  console.log(`${socket.id} connected`);
+  socket.broadcast.emit('newConnection', socket.id);
+
+  socket.on('msg', (msg) => {
+    console.log(`New message from ${socket.id}: ${msg}`);
+    socket.broadcast.emit('newMsg', { value: msg, sender: socket.id });
+  })
+});
+
+server.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
